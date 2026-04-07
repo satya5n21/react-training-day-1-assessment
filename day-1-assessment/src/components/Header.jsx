fbec35d08000bc557f3ecdf5e12795070bf6300a
Satyajit Nayak <77431114+satya5n21@users.noreply.github.com>
2026-04-07 12:45:07 +0530

    day 1 assessment added

---
+++ day-1-assessment/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// Custom Component Header -----------
+function Header() {
+  return (
+    <header className="main-header">
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
